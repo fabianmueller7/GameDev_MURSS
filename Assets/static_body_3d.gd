ff5f15f11aef72fa9f48ca6ec01d91189e662500
fabian.mueller <fabian.mueller@stud.hslu.ch>
2026-09-26 16:23:11 +0200

    added main character and light

---
+++ Assets/static_body_3d.gd
@@ -0,0 +1,13 @@
+extends StaticBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var mat := StandardMaterial3D.new()
+	mat.albedo_color = Color.RED
+	mesh.material_override = mat
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
